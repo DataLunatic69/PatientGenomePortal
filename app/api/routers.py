55b228d8bafaaef56e70e95136bdd577c3cde746
DataLunatic69 <amansingh2002.ap@gmail.com>
2026-03-22 16:50:02 +0530

    fixinf routes

---
+++ app/api/routers.py
@@ -0,0 +1,21 @@
+from fastapi import APIRouter
+
+from app.api.endpoints.v1 import upload, analysis, variants
+
+api_router = APIRouter(prefix="/api/v1")
+
+api_router.include_router(
+    upload.router,
+    prefix="/upload",
+    tags=["upload"],
+)
+api_router.include_router(
+    analysis.router,
+    prefix="/jobs",
+    tags=["analysis"],
+)
+api_router.include_router(
+    variants.router,
+    prefix="/variants",
+    tags=["variants"],
+)
