fd594758c231fc3d4179dd9042f435774fbb0f09
DataLunatic69 <amansingh2002.ap@gmail.com>
2026-03-23 23:02:24 +0530

    resolved path issue with routers

--- app/api/routers.py
+++ app/api/routers.py
@@ -1,9 +1,14 @@
 from fastapi import APIRouter
 
-from app.api.endpoints.v1 import upload, analysis, variants
+from app.api.endpoints.v1 import upload, analysis, variants, auth
 
 api_router = APIRouter(prefix="/api/v1")
 
+api_router.include_router(
+    auth.router,
+    prefix="/auth",
+    tags=["auth"],
+)
 api_router.include_router(
     upload.router,
     prefix="/upload",
